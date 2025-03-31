af255f29661cac983444c18b7e63a372a31d3ba4
[Sergio-Dannunzio] <sergiodannunzio@hotmail.com>
2025-03-31 13:54:03 -0300

    added style to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,20 @@ import JobCard from "../components/JobCard";
 export default function Home() {
     return(    
         <div>
-            <p>assd</p>
-            <JobCard></JobCard>
+            <div className="p-6 mb-2">
+                <h1 className="text-3xl">Trabajos</h1>
+            </div>
+            <div className="flex justify-between px-8">
+                <h2 className="text-xl">Sin respuesta</h2>
+                <h2 className="text-xl">Con respuesta</h2>
+                <h2 className="text-xl">Rechazados</h2>
+            </div>
+            <div className="p-4 px-10">
+                <JobCard></JobCard>
+                <JobCard></JobCard>
+                <JobCard></JobCard>
+                <JobCard></JobCard>
+            </div>
         </div>
     )
 }

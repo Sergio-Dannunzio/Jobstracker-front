8c0d63a33bf498b9664713c857e47c28342b9e0d
[Sergio-Dannunzio] <sergiodannunzio@hotmail.com>
2025-03-31 13:42:56 -0300

    added check to expire time token

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -3,7 +3,7 @@ import { FaComment } from "react-icons/fa";
 export default function JobCard() {
     return(
         <div className="flex flex-col bg-[#ffffffbf] max-w-72 rounded-2xl p-4">
-            <div className="mb-2 border-b-2 border-[#b978ff9d] flex justify-between items-center">
+            <div className="mb-2 border-b-2 border-[#b978ff9d] flex justify-between items-center pb-2">
                 <h1>Title</h1>
                 <h2 className="px-2 text-sm text-center text-green-700 bg-green-300 rounded-3xl">Status</h2>
             </div>
--- src/router/PrivateRoute.tsx
+++ src/router/PrivateRoute.tsx
@@ -1,9 +1,23 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+
+const isValidToken = (token: string | null): boolean => {
+  if (!token) return false;
+
+  try {
+    const decoded: { exp: number } = jwtDecode(token);
+    const now = Date.now() / 1000; // Tiempo actual en segundos
+    return decoded.exp > now; // Devuelve true si el token no ha expirado
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return false; // Token inválido o mal formado
+  }
+};
 
 const PrivateRoute: React.FC = () => {
   const token = localStorage.getItem("token");
 
-  return token ? <Outlet /> : <Navigate to="/login" />;
+  return isValidToken(token) ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;

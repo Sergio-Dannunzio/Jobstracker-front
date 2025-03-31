55b0a3fff81a687630efdd454e2d0db0b3c082f6
[Sergio-Dannunzio] <sergiodannunzio@hotmail.com>
2025-03-31 13:55:43 -0300

    added style to jobcard

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -2,9 +2,9 @@ import { FaComment } from "react-icons/fa";
 
 export default function JobCard() {
     return(
-        <div className="flex flex-col bg-[#ffffffbf] max-w-72 rounded-2xl p-4">
-            <div className="mb-2 border-b-2 border-[#b978ff9d] flex justify-between items-center pb-2">
-                <h1>Title</h1>
+        <div className="flex flex-col bg-[#ffffffbf] max-w-72 rounded-2xl p-4 my-2">
+            <div className="mb-2 border-b-2 border-[#b978ff9d] flex justify-between items-center pb-1">
+                <h1 className="text-lg">Title</h1>
                 <h2 className="px-2 text-sm text-center text-green-700 bg-green-300 rounded-3xl">Status</h2>
             </div>
             <div className="mb-2">

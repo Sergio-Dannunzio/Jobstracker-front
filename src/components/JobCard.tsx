eee27cdd16ff407e4bd3867a6df80aa9046ec96f
[Sergio-Dannunzio] <sergiodannunzio@hotmail.com>
2025-04-03 13:55:45 -0300

    added style to status in card

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -6,7 +6,18 @@ const JobCard: React.FC<Job> = ({ name, status, desc }) => {
     return(
         <Card>
             <CardHeader>
-                <CardTitle className="flex justify-between">{name} <h2 className="px-2 text-sm text-center text-green-700 bg-green-300 rounded-3xl h-fit">{status}</h2></CardTitle>
+                <CardTitle className="flex justify-between">
+                    {name}
+                    {status === "enviado" &&
+                        <h2 className="px-2 text-sm text-center text-green-700 bg-green-300 rounded-3xl h-fit">{status}</h2>
+                    }
+                    {status === "respondido" &&
+                        <h2 className="px-2 text-sm text-center text-yellow-700 bg-yellow-300 rounded-3xl h-fit">{status}</h2>
+                    }
+                    {status === "rechazado" &&
+                        <h2 className="px-2 text-sm text-center text-red-700 bg-red-300 rounded-3xl h-fit">{status}</h2>
+                    }
+                </CardTitle>
             </CardHeader>
             <CardContent>
                 <p>{desc}</p>
